27838fc6e1491309d9a62f626a12b0542b39e0fe
M0BiN <veryhighsigh@gmail.com>
2020-03-30 18:54:26 +0430

    Updates

---
+++ precache-manifest.01ee8709c1c971bdb3ea1dbd0ef98e33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4618c758803d6eeb69da5436c70352b2",
+    "url": "/flappy-voice/index.html"
+  },
+  {
+    "revision": "11a8c3c6362fa2a99592",
+    "url": "/flappy-voice/static/css/main.75de3ee2.chunk.css"
+  },
+  {
+    "revision": "75e342c2e7bd2708ed98",
+    "url": "/flappy-voice/static/js/2.34a5a3b7.chunk.js"
+  },
+  {
+    "revision": "fa2f71d26dd7cdf5520a47775ab0cae9",
+    "url": "/flappy-voice/static/js/2.34a5a3b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a8c3c6362fa2a99592",
+    "url": "/flappy-voice/static/js/main.4617b13b.chunk.js"
+  },
+  {
+    "revision": "c1ce4fd9382272f12589",
+    "url": "/flappy-voice/static/js/runtime-main.0fadff9b.js"
+  },
+  {
+    "revision": "1f3aa776cc88bfe8526b4e0ae81792e3",
+    "url": "/flappy-voice/static/media/bird1.1f3aa776.png"
+  },
+  {
+    "revision": "7b191a8d7a49bc458e244b5fc6075c41",
+    "url": "/flappy-voice/static/media/bird2.7b191a8d.png"
+  },
+  {
+    "revision": "f0723ad5c17e1be9657a2d5bb86255e6",
+    "url": "/flappy-voice/static/media/bird3.f0723ad5.png"
+  },
+  {
+    "revision": "2acd3c5c368f73a87aa5154fc9a10b68",
+    "url": "/flappy-voice/static/media/flappybirdBackground.2acd3c5c.jpg"
+  },
+  {
+    "revision": "ac67fe34b12c11dd5b78f2fd2c7a0e5b",
+    "url": "/flappy-voice/static/media/flappybird_SkyBuilding.ac67fe34.jpg"
+  }
+]);
